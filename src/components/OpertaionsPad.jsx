8998fa37e78dd2d7eea7f869b43fef96e50d36eb
Saif Eddine <saif.al-kallaf@loyaltylogistix.com>
2022-05-21 14:49:58 +0200

    Feat - Test the add of + sign to the display pad to work properly and avoid duplications

--- src/components/OpertaionsPad.jsx
+++ src/components/OpertaionsPad.jsx
@@ -1,12 +1,30 @@
+import { useContext } from "react";
 import { operations } from "../constants";
+import AppContext from "../context";
 import "../App.css";
 
 function OpertaionsPad() {
+  const { result, setResult } = useContext(AppContext);
+  // Create a function to check if the plus can be added or not and then
+  // display the current operation properly
+  const handleClick = (label) => {
+    if (result === 0) {
+      return;
+    }
+    if (String(result).endsWith("+")) {
+      return;
+    }
+    setResult(result + label);
+  };
   return (
     <div className="operations-pad">
       {operations.map(({ label }) => {
         return (
-          <span key={label} className="clicked">
+          <span
+            key={label}
+            className="clicked"
+            onClick={() => handleClick(label)}
+          >
             {label}
           </span>
         );
